198ef98a988c57dda3ce51604f3fbf5d62ce6bba
isa486 <aczol2424@gmail.com>
2025-07-17 09:42:17 -0600

    Delete backend directory

--- backend/models/maquina.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const MaquinaSchema = new mongoose.Schema({
-  id: { type: String, required: true, unique: true },
-  nombre: { type: String, required: true },
-  qr: String
-});
-
-export default mongoose.model("Maquina", MaquinaSchema);
